c4c4ea8bb6460ef733b22018054b5552f6175294
Martin Brochhaus <mbrochh@gmail.com>
2012-12-17 19:22:23 +0800

    Added save_setting and get_setting methods to the base widget class.

--- metrics_dashboard/tests/widget_base_tests.py
+++ metrics_dashboard/tests/widget_base_tests.py
@@ -9,6 +9,8 @@ from metrics_dashboard.tests.test_widget_app.dashboard_widgets import (
 
 class DashboardWidgetBaseTestCase(TestCase):
     """Tests for the ``DashboardWidgetBase`` base class."""
+    longMessage = True
+
     def test_get_context_data(self):
         """get_context_data should return an empty dictionary by default."""
         base = DashboardWidgetBase()
@@ -28,3 +30,31 @@ class DashboardWidgetBaseTestCase(TestCase):
         """update_widget_data should throw exception if not implemented."""
         base = DashboardWidgetBase()
         self.assertRaises(NotImplementedError, base.update_widget_data)
+
+    def test_save_setting(self):
+        """save_setting should save the value to the database."""
+        widget = DummyWidget()
+        setting = widget.save_setting('IS_ENABLED', '1')
+        self.assertTrue(setting.pk, msg=(
+            'Should create a new DB entry when saving the setting for the'
+            ' first time'))
+        self.assertEqual(setting.value, '1', msg=(
+            'Should set the correct value on the new setting object'))
+
+        setting2 = widget.save_setting('IS_ENABLED', '0')
+        self.assertEqual(setting, setting2, msg=(
+            'Should not create a new object if that setting already exists'
+            ' in the database'))
+        self.assertEqual(setting2.value, '0', msg=(
+            'Should update the setting value on save'))
+
+    def test_get_setting(self):
+        """get_setting should get the setting from the database."""
+        widget = DummyWidget()
+        setting = widget.get_setting('IS_ENABLED')
+        self.assertEqual(setting, None, msg=(
+            'Should return None if the setting does not exist in the db'))
+        widget.save_setting('IS_ENABLED', '1')
+        setting = widget.get_setting('IS_ENABLED')
+        self.assertEqual(setting.setting_name, 'IS_ENABLED', msg=(
+            'Should return the correct setting from the database when called'))
--- metrics_dashboard/widget_base.py
+++ metrics_dashboard/widget_base.py
@@ -1,8 +1,20 @@
 """Base DashboardWidget of the ``django-metrics-dashboard`` app."""
+from django.utils.translation import ugettext_lazy as _
+
+from metrics_dashboard.models import DashboardWidgetSettings
 
 
 class DashboardWidgetBase(object):
     """All DashboardWidgets must inherit this base class."""
+    base_settings = {
+        'IS_ENABLED': {
+            'verbose_name': _('Is enabled'),
+        },
+        'LAST_UPDATE': {
+            'verbose_name': _('Last update'),
+        }
+    }
+
     def get_context_data(self):
         """
         Should return a dictionary of template context variables.
@@ -13,6 +25,28 @@ class DashboardWidgetBase(object):
     def get_name(self):
         """Returns the class name of this widget."""
         return self.__class__.__name__
+
+    def get_setting(self, setting_name):
+        """Returns the setting for this widget from the database."""
+        try:
+            setting = DashboardWidgetSettings.objects.get(
+                widget_name=self.get_name(),
+                setting_name=setting_name)
+        except DashboardWidgetSettings.DoesNotExist:
+            setting = None
+        return setting
+
+    def save_setting(self, setting_name, value):
+        """Saves the setting value into the database."""
+        setting = self.get_setting(setting_name)
+        if setting is None:
+            setting = DashboardWidgetSettings.objects.create(
+                widget_name=self.get_name(),
+                setting_name=setting_name,
+                value=value)
+        setting.value = value
+        setting.save()
+        return setting
 
     def update_widget_data(self):
         """
