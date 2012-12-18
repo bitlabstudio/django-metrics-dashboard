3e3217a8baab7d8c723fc4a7cc9d421c32597646
Martin Brochhaus <mbrochh@gmail.com>
2012-12-18 16:21:42 +0800

    Added mock so that tests run through again.

--- metrics_dashboard/tests/update_widget_data_tests.py
+++ metrics_dashboard/tests/update_widget_data_tests.py
@@ -2,9 +2,18 @@
 from django.core.management import call_command
 from django.test import TestCase
 
+from mock import patch
+
+from metrics_dashboard.widget_pool import dashboard_widget_pool
+
 
 class UpdateWidgetDataTestCase(TestCase):
     """Tests for the ``update_widget_data`` admin command."""
     def test_command(self):
         """update_widget_data should run without errors"""
-        call_command('update_widget_data')
+        widget = dashboard_widget_pool.widgets['DummyWidget']
+        with patch.object(
+            widget, 'update_widget_data', return_value=None) as mock_method:
+
+            call_command('update_widget_data')
+        mock_method.assert_called_once()
