dc5c29d1e91acd8f5a0793413aa7ff09c88830a3
Martin Brochhaus <mbrochh@gmail.com>
2013-02-04 15:03:07 +0800

    Added new setting UPDATE_WIDGET_DATA_LOCKFILE

--- metrics_dashboard/management/commands/update_widget_data.py
+++ metrics_dashboard/management/commands/update_widget_data.py
@@ -1,4 +1,5 @@
 """Custom admin command that updates the data for all registered widgets."""
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from django_libs.decorators import lockfile
@@ -9,7 +10,8 @@ from metrics_dashboard.widget_pool import dashboard_widget_pool
 class Command(BaseCommand):
     help = 'Updates the data for all registered widgets.'
 
-    @lockfile('update_widget_data')
+    @lockfile(getattr(
+        settings, 'UPDATE_WIDGET_DATA_LOCKFILE', 'update_widget_data'))
     def handle(self, *args, **options):
         for widget_name, widget in dashboard_widget_pool.get_widgets().items():
             if widget.should_update():
